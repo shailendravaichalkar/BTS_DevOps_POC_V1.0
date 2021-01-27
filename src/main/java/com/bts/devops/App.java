6be3065533692ad198b58d6a926fe71b3a0950af
Shailendra Vaichalkar <shailendra.v@fiserv.com>
2021-01-27 07:38:03 +0000

    Hello Added

--- src/main/java/com/bts/devops/App.java
+++ src/main/java/com/bts/devops/App.java
@@ -10,6 +10,6 @@ public class App
     public static void main( String[] args )
     {
         // This is buggy line. 
-        System.out.println( "This is DevOps Demo!" );
+        System.out.println( "Hello Priyanka!" );
     }
 }
