f01f52edcf4a8d06f377aeb308b5663c26163e7b
Shailendra Vaichalkar <shailendra.v@fiserv.com>
2021-01-27 07:59:34 +0000

    code changes

--- src/main/java/com/bts/devops/App.java
+++ src/main/java/com/bts/devops/App.java
@@ -10,6 +10,6 @@ public class App
     public static void main( String[] args )
     {
         // This is buggy line. 
-        System.out.println( "Hello Priyanka!" );
+        System.out.println( "Hello World!" );
     }
 }
